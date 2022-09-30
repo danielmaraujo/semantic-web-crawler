d6bcb2b55a79cf2bee923a26c0cc5d82d06dc99b
danielmaraujo <danielaraujoas@gmail.com>
2022-09-30 15:01:06 -0300

    Add SemanticCrawler interface

---
+++ src/main/java/impl/SemanticCrawlerImpl.java
@@ -0,0 +1,12 @@
+package impl;
+
+import crawler.SemanticCrawler;
+import org.apache.jena.rdf.model.Model;
+
+public class SemanticCrawlerImpl implements SemanticCrawler {
+    @Override
+    public void search(Model graph, String resourceURI) {
+
+
+    }
+}
